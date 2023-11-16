8f409ec1640a1d86af7f03b7a5f70baf53e975dd
Rodrigo Alvarez <rodri122004@gmail.com>
2023-11-16 08:44:21 -0600

    subiendo archivos.

---
+++ Semana 9/Actividad 9 REAM1253323.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T9_REAM_1253323
+{
+    class Motocicleta
+    {
+        private string modelo;
+        private string marca;
+        private decimal precio;
+        private decimal porcentajeIVA;
+
+        public void SetDatos(string modelo, string marca, decimal precio, decimal porcentajeIVA)
+        {
+            this.modelo = modelo;
+            this.marca = marca;
+            this.precio = precio;
+            this.porcentajeIVA = porcentajeIVA;
+        }
+
+        public string GetDatos()
+        {
+            return $"Modelo: {modelo}\nMarca: {marca}\nPrecio: {precio:C}\nPorcentaje de IVA: {porcentajeIVA}%";
+        }
+
+        public decimal PrecioSinIVA()
+        {
+            return precio;
+        }
+
+        public decimal PrecioConIVA()
+        {
+            return precio * (1 + porcentajeIVA / 100);
+        }
+
+        public decimal MontoIVA()
+        {
+            return precio * (porcentajeIVA / 100);
+        }
+    }
+}
